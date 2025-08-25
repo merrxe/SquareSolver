b6fc9194548141b61b95fc75aede620e1ea992db
merrxe <qmary2007q@gmail.com>
2025-08-25 21:28:01 +0300

    переписала тестер
    добавила структуры в тестер, написала makefile

--- Tester.cpp
+++ Tester.cpp
@@ -3,40 +3,74 @@
 #include "Tester.h"
 #include "SquareSolver.h"
 
-int OneTest (double a, double b, double c, roots nRoots_ref, double x1_ref, double x2_ref);
+struct CoeffSquare
+    {
+    double a, b, c;
+    };
 
+struct RootsSquare
+    {
+    double x1_ref, x2_ref;
+    };
+
+
+struct SquareTestData
+    {
+    struct CoeffSquare coeff;
+    roots nRoots_ref;
+    struct RootsSquare roots_ref;
+    };
+
+int OneTest (SquareTestData);
 
 int nFailed ()
     {
     int failed = 0;
-    /*ТЕСТЫ ДЛЯ ДВУХ КОРНЕЙ*/
-    failed += OneTest(1, -5, 6, TWO_ROOTS, 2, 3); // ax^2 + bx + c = 0, D > 0
-    failed += OneTest(1, 0, -4, TWO_ROOTS, -2, 2); // ax*2 + c = 0
-    failed += OneTest(1, -5, 0, TWO_ROOTS, 0, 5); // ax^2 + bx = 0
 
-    /*ТЕСТЫ ДЛЯ ОДНОГО КОРНЯ*/
-    failed += OneTest(1, -10, 25, ONE_ROOT, 5, 5); // ax^2 + bx + c = 0, D = 0
-    failed += OneTest(0, 5, -10, ONE_ROOT, 2, 2); // bx + c = 0
+    SquareTestData sqTests [] =
+        {
+        /*ТЕСТЫ ДЛЯ ДВУХ КОРНЕЙ*/
+        {.coeff = {.a = 1, .b = -5, .c = 6},  .nRoots_ref = TWO_ROOTS, .roots_ref = {.x1_ref = 2,  .x2_ref = 3}},
+        {.coeff = {.a = 1, .b = 0,  .c = -4}, .nRoots_ref = TWO_ROOTS, .roots_ref = {.x1_ref = -2,  .x2_ref = 2}},
+        {.coeff = {.a = 1, .b = -5, .c = 0},  .nRoots_ref = TWO_ROOTS, .roots_ref = {.x1_ref = 0,  .x2_ref = 5}},
 
-    /*ТЕСТ ДЛЯ ОТСУТСТВИЯ ДЕЙСТВИТЕЛЬНЫХ РЕШЕНИЙ*/
-    failed += OneTest(100, 2, 100, NO_REAL_SOLUTION, 0, 0);
+        /*ТЕСТЫ ДЛЯ ОДНОГО КОРНЯ*/
+        {.coeff = {.a = 1, .b = -10, .c = 25},  .nRoots_ref = ONE_ROOT, .roots_ref = {.x1_ref = 5,  .x2_ref = 5}},
+        {.coeff = {.a = 0, .b = 5, .c = -10}, .nRoots_ref = ONE_ROOT, .roots_ref = {.x1_ref = 2,  .x2_ref = 2}},
 
-    /*ТЕСТ ДЛЯ ОТСУТСТВИЯ КОРНЕЙ*/
-    failed += OneTest(0, 0, 5, NO_ROOTS, 0, 0);
+        /*ТЕСТ ДЛЯ ОТСУТСТВИЯ ДЕЙСТВИТЕЛЬНЫХ РЕШЕНИЙ*/
+        {.coeff = {.a = 100, .b = 2, .c = 100}, .nRoots_ref = NO_REAL_SOLUTION, .roots_ref = {.x1_ref = 0,  .x2_ref = 0}},
 
-    /*ТЕСТ ДЛЯ БЕСКОНЕЧНОГО КОЛИЧЕСТВА РЕШЕНИЙ*/
-    failed += OneTest(0, 0, 0, INFINITE_ROOTS, 0, 0);
+        /*ТЕСТ ДЛЯ ОТСУТСТВИЯ КОРНЕЙ*/
+        {.coeff = {.a = 0, .b = 0, .c = 5}, .nRoots_ref = NO_ROOTS, .roots_ref = {.x1_ref = 0,  .x2_ref = 0}},
 
-    return failed;
+        /*ТЕСТ ДЛЯ БЕСКОНЕЧНОГО КОЛИЧЕСТВА РЕШЕНИЙ*/
+        {.coeff = {.a = 0, .b = 0, .c = 0}, .nRoots_ref = INFINITE_ROOTS, .roots_ref = {.x1_ref = 0, .x2_ref = 0}},
+        };
+
+        size_t size = sizeof (sqTests) / sizeof (SquareTestData);
+
+        for (size_t i = 0; i < size; i++)
+            {
+            failed += OneTest (sqTests[i]);
+            }
+
+        return failed;
     }
 
-int OneTest (double a, double b, double c, roots nRoots_ref, double x1_ref, double x2_ref)
+
+int OneTest (SquareTestData dataOneTest)
     {
     double x1 = 0, x2 = 0;
+
+    double a = dataOneTest.coeff.a, b = dataOneTest.coeff.b, c = dataOneTest.coeff.c;
+    roots nRoots_ref = dataOneTest.nRoots_ref;
+    double x1_ref = dataOneTest.roots_ref.x1_ref, x2_ref = dataOneTest.roots_ref.x2_ref;
+
     int nRoots = SquareSolve (a, b, c, &x1, &x2);
     if (!(IsEqual (x1, x1_ref)  && IsEqual (x2, x2_ref) && nRoots == nRoots_ref))
         {
-        printf ("При решении квадратного урвнения с коэффициентами a = %.2f, b = %.2f, c = %.2f получены корни %.2f и %.2f, ожидаемые корни %.2f и %.2f. \n", a, b, c, x1, x2, x1_ref, x2_ref);
+        printf ("При решении квадратного уравнения с коэффициентами a = %.2f, b = %.2f, c = %.2f получены корни %.2f и %.2f, ожидаемые корни %.2f и %.2f. \n", a, b, c, x1, x2, x1_ref, x2_ref);
         return 1;
         }
     return 0;
